d35584d9d1963a5533b4adacf343bb40c3241183
Divi22005 <23r11a66b8@gcet.edu.in>
2025-05-29 18:47:06 +0530

    updated

---
+++ q3.js
@@ -0,0 +1,14 @@
+db.products.insertMany([
+  {
+    name: "asus laptop",
+    Image: "https://tse2.mm.bing.net/th?id=OIP.BVgxFfjh-GHJ-5ELMLaoQgHaHa&pid=Api&P=0&h=180",
+    price: 1200,
+    discription: "ASUS laptops are known for their innovative design, reliable performance, and affordability. They cater to a wide range of users, from students and professionals to gamers and creators.",
+  },
+  {
+    name: "Samsung Galaxy S23",
+    Image: "https://tse3.mm.bing.net/th?id=OIP.5QMkXIlusvZfEUm50okfbAHaHa&pid=Api&P=0&h=180",
+    price: 999,
+    discription: "The Samsung Galaxy S23 is a flagship smartphone that offers cutting-edge technology, stunning design, and powerful performance. It features a high-resolution display, advanced camera system, and long-lasting battery life.",
+  },
+]);
