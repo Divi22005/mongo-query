be325641dc069e4485bd89bdfdf937a6d7fb5635
Divi22005 <23r11a66b8@gcet.edu.in>
2025-05-31 19:07:32 +0530

    updated

---
+++ q14.js
@@ -0,0 +1,30 @@
+db.employees.aggregate([
+  {
+    $lookup: {
+      from: "orders",           
+      localField: "empId",       
+      foreignField: "empId",     
+      as: "orders"               
+    }
+  },
+  {
+    $unwind: "$orders"          
+  },
+  {
+    $group: {
+      _id: {
+        name: "$name",
+        email: "$email"
+      },
+      totalOrderValue: { $sum: "$orders.orderValue" }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      name: "$_id.name",
+      email: "$_id.email",
+      totalOrderValue: 1
+    }
+  }
+]);
